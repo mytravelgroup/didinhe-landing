74fad2d72f11bbf2ce842e0e619237a5add9fc42
koheejs <dangkhoibr1992@gmail.com>
2024-05-10 15:38:51 +0700

    implement universal link page

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,6 +3,23 @@
 /**
  * Init custom functions
  */
-(function initWelcomMessage() {
+(function initWelcomeMessage() {
   console.log('didinhe landing page init successfully');
+})();
+
+/**
+ * Init universal function
+ */
+(function initUniversalFunction() {
+  const APP_TRIP_URL = 'didinhe://trip_details?tripId=';
+  const linkButton = document.getElementById('open-app-btn');
+  if (!linkButton) return;
+
+  // get the app url and params from address bar
+  const tripId = new URLSearchParams(window.location.search).get('tripId');
+  if (!tripId) return;
+
+  const tripDetailUrl = `${APP_TRIP_URL}${tripId}`;
+  linkButton.setAttribute('href', tripDetailUrl);
+  linkButton.click();
 })();
